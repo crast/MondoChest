7a9023aaf68930c9ecd48d69ddebe7cbf14d673c
crast <>
2013-02-04 19:52:19 -0700

    Mark version 0.6.2

--- src/us/crast/mondochest/MondoConstants.java
+++ src/us/crast/mondochest/MondoConstants.java
@@ -2,7 +2,7 @@ package us.crast.mondochest;
 
 public final class MondoConstants {
 	public static final String APP_NAME = "MondoChest";
-	public static final String MONDOCHEST_VERSION = "0.6.2-pre1";
+	public static final String MONDOCHEST_VERSION = "0.6.2";
 	
 	public static final String MASTER_SIGN_NAME = "[MondoChest]";
 	public static final String SLAVE_SIGN_NAME = "[MondoSlave]";
