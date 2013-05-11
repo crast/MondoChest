aa76623dceed3fa68a48571801697cc92d821361
crast <contact@jamescrasta.com>
2013-05-11 13:30:58 -0600

    Be more resilient in checking/setting last clicked location

--- src/main/java/us/crast/mondochest/persist/PlayerState.java
+++ src/main/java/us/crast/mondochest/persist/PlayerState.java
@@ -2,20 +2,25 @@ package us.crast.mondochest.persist;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class PlayerState {
-	private String lastClickedWorld;
-	private Vector lastClickedVector;
+	private String lastClickedWorld = null;
+	private Vector lastClickedVector = null;
 	
 	private State state = State.NORMAL;
+	
 	public PlayerState(Player player) {}
 	
 	/* Getters/Setters */
 	public Location getLastClickedMaster() {
+	    if (lastClickedWorld == null) return null;
+	    World world = Bukkit.getWorld(lastClickedWorld);
+	    if (world == null) return null;
 		return new Location(
-		    Bukkit.getWorld(lastClickedWorld),
+		    world,
 		    lastClickedVector.getX(),
 		    lastClickedVector.getY(),
 		    lastClickedVector.getZ()
@@ -23,8 +28,12 @@ public class PlayerState {
 	}
 	
 	public void setLastClickedMaster(Location lastClickedMaster) {
-		this.lastClickedWorld = lastClickedMaster.getWorld().toString();
-		this.lastClickedVector = lastClickedMaster.toVector();
+	    if (lastClickedMaster == null) {
+	        this.lastClickedWorld = null;
+	    } else {
+    		this.lastClickedWorld = lastClickedMaster.getWorld().toString();
+    		this.lastClickedVector = lastClickedMaster.toVector();
+	    }
 	}
 	
 	public boolean isManagingChest() {
