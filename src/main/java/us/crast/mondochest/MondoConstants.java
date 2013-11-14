d757477eb6029bea96c9109beeb8cd3ec2f434b8
James Crasta <james@crasta.com>
2013-11-14 01:11:20 -0700

    tip is now 0.7.2-pre1

--- src/main/java/us/crast/mondochest/MondoConstants.java
+++ src/main/java/us/crast/mondochest/MondoConstants.java
@@ -8,7 +8,7 @@ import mondocommand.ChatMagic;
 
 public final class MondoConstants {
 	public static final String APP_NAME = "MondoChest";
-	public static final String MONDOCHEST_VERSION = "0.7.1";
+	public static final String MONDOCHEST_VERSION = "0.7.2-pre1";
 
 	public static final int DOUBLE_CHEST_SIZE = 54;
 	public static final int SINGLE_CHEST_SIZE = 27;
